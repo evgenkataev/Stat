0c3f6b0d9c5bd2a7bfe42dc8373132ceb03f3ef9
evgenkataev <evgenkataev@gmail.com>
2022-06-13 21:58:13 +0500

    second commit

--- src/main/java/ru/netology/stats/Stat/Statservice.java
+++ src/main/java/ru/netology/stats/Stat/Statservice.java
@@ -1,2 +1,29 @@
-package ru.netology.stats.Stat;public class Statservice {
+package ru.netology.stats.Stat;
+
+public class Statservice {
+    public int minSales(int[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public int maxSales(int[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
 }
--- src/test/java/ru/netology/stats/Stat/StatserviceTest.java
+++ src/test/java/ru/netology/stats/Stat/StatserviceTest.java
@@ -1,2 +1,26 @@
-package ru.netology.stats.Stat;public class StatserviceTest {
+package ru.netology.stats.Stat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testng.asserts.Assertion;
+
+public class StatserviceTest {
+    @Test
+    public void DayOfMinSales() {
+        Statservice service = new Statservice();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedDay = 9;
+        int actualDay = service.minSales(sales);
+        Assertions.assertEquals(expectedDay,actualDay);
+        System.out.println(actualDay);
+
+    }
+    @Test
+    public void DayOfMaxSales() {
+        Statservice service = new Statservice();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedDay = 9;
+        int actualDay = service.minSales(sales);
+        Assertions.assertEquals(expectedDay,actualDay);
+        System.out.println(actualDay);}
 }
